3da0a7531f5388285832b4321d5b9708e83954d5
nachopedraza1 <nachi_choco@live.com.ar>
2023-10-17 14:12:50 -0300

    fix history

--- components/tables/HistoryStats.tsx
+++ components/tables/HistoryStats.tsx
@@ -26,19 +26,19 @@ export const HistoryStats: FC<Props> = ({ history, totalDonated }) => {
 
             <Grid container justifyContent="center" gap={3} mb={4}>
                 <Grid item xs={12} md={3} className='top-box'>
-                    <Typography variant='h6' mb={3} noWrap> Numero de aportes: </Typography>
+                    <Typography variant='h5' mb={3} noWrap> Numero de aportes: </Typography>
                     <Box textAlign="center" mt={2}>
                         <Typography variant="h5" color="primary.main"> {history.length} </Typography>
                     </Box>
                 </Grid>
                 <Grid item xs={12} md={3} className='top-box'>
-                    <Typography variant='h6' mb={3} noWrap> Total aportado: </Typography>
+                    <Typography variant='h5' mb={3} noWrap> Total aportado: </Typography>
                     <Box textAlign="center" mt={2}>
                         <Typography variant="h5" color="primary.main">{totalDonated}</Typography>
                     </Box>
                 </Grid>
                 <Grid item xs={12} md={3} className='top-box'>
-                    <Typography variant='h6' mb={3} noWrap> Ultimo aporte: </Typography>
+                    <Typography variant='h5' mb={3} noWrap> Ultimo aporte: </Typography>
                     <Box textAlign="center" mt={2}>
                         <Typography variant="h5" color="primary.main">{history[0].createdAt}</Typography>
                     </Box>
